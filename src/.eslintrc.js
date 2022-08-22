cbc5612f15290bb6fd1ab23072297665deac3b21
piyokintv <kmori0050@gmail.com>
2022-08-22 17:50:43 +0900

    eslintのルールを記述するための.eslintrc.jsを作成

---
+++ src/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 'env': {
+  //   'browser': true,
+  //   'es2021': true
+  // },
+  'extends': [
+    // 'airbnb-base',
+    'plugin:vue/vue3-recommended',
+    // '.eslintrc-todo.json'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  // 'plugins': [
+  //   'vue'
+  // ],
+  'rules': {
+  }
+};
