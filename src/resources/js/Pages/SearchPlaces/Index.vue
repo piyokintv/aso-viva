87287c0abf0a202c094ca08338186cdeb51c233f
piyokintv <kmori0050@gmail.com>
2023-01-12 18:00:54 +0900

    行きたい場所入力用のlabelとtextareaを設置

--- src/resources/js/Pages/SearchPlaces/Index.vue
+++ src/resources/js/Pages/SearchPlaces/Index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    サンプル表示
+  <div class="mb-3">
+    <label
+      for="exampleFormControlTextarea1"
+      class="form-label"
+    >
+      Example textarea
+    </label>
+    <textarea
+      id="exampleFormControlTextarea1"
+      class="form-control"
+      rows="3"
+      placeholder="東京駅、東京タワー、東京スカイツリー"
+    />
   </div>
 </template>
 
